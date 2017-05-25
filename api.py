9fabdffe366f877b39e9c255a1eeb04672323660
aguardia13 <er_guardia@hotmail.com>
2017-05-25 18:38:34 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -5,15 +5,15 @@ import MySQLdb
 app = Flask(__name__)
 
 db = MySQLdb.connect(host="localhost", user="root", passwd="toor", db="magikproyectBD")
-cursor = db.cursor()
 
 
-@app.route('/get/usuario', methods=['GET'])
-def obtener_usuario():
+@app.route('/get/emails', methods=['GET'])
+def obtener_emails():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		email = r.get("email")
-		sql = """SELECT * FROM Usuario WHERE email="""+etadomail
+		sql = """SELECT email FROM Usuario;"""
 		cursor.execute(sql)
 		resultados = cursor.fetchone()
 		ids = resultados[0]
@@ -24,19 +24,45 @@ def obtener_usuario():
 		imagenPerfil = resultados[5]
 
 		data={"id":ids,"nombre":nombre, "apellidos":apellidos, "email":email, "password":password, "admin":admin, "imagenPerfil":imagenPerfil}
-		db.close()
+		cursor.close()
 		return json.dumps(data)
 	except:
-		db.close()
+		cursor.close()
+		return "Unable to insert data on database."
+
+
+
+@app.route('/get/usuario', methods=['GET'])
+def obtener_usuario():
+	cursor = db.cursor()
+	try:
+		r = request.get_json()
+		email = r.get("email")
+		sql = """SELECT * FROM Usuario WHERE email="""+email+""";"""
+		cursor.execute(sql)
+		resultados = cursor.fetchone()
+		ids = resultados[0]
+		nombre = resultados[1]
+		apellidos = resultados[2]
+		password = resultados[3]
+		admin = resultados[4]
+		imagenPerfil = resultados[5]
+
+		data={"id":ids,"nombre":nombre, "apellidos":apellidos, "email":email, "password":password, "admin":admin, "imagenPerfil":imagenPerfil}
+		cursor.close()
+		return json.dumps(data)
+	except:
+		cursor.close()
 		return "Unable to insert data on database."
 
 
 @app.route('/get/incidencias', methods=['GET'])
 def obtener_incidencias():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		email = r.get("email")
-		sql = """SELECT * FROM Incidencias WHERE email="""+email
+		sql = """SELECT * FROM Incidencias WHERE email="""+email+""";"""
 		cursor.execute(sql)
 		resultados = cursor.fetchone()
 		ids= resultados[0]
@@ -47,10 +73,10 @@ def obtener_incidencias():
 		longitud = resultados[5]
 
 		data={"id":ids, "descripcion":descripcion, "direccion":direccion, "imagen":imagen, "latitud":latitud, "longitud":longitud, "email":email}
-		db.close()
+		cursor.close()
 		return json.dumps(data)
 	except:
-		db.close()
+		cursor.close()
 		return "Unable to insert data on database."
 
 
@@ -58,6 +84,7 @@ def obtener_incidencias():
 
 @app.route('/post/usuario', methods=['POST'])
 def insertar_usuario():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		nombre = r.get("nombre")
@@ -66,19 +93,21 @@ def insertar_usuario():
 		password = r.get("password")
 		admin = r.get("admin")
 		imagenPerfil = r.get("imagenPerfil")
-		sql = """INSERT INTO Usuario (nombre, apellidos, email, password, admin, imagenPerfil) VALUES ('"""+nombre+"""', '"""+apellidos+"""', '"""+email+"""', '"""+password+"""', '"""+admin+"""', '"""+imagenPerfil+"""')"""
+		sql = """INSERT INTO Usuario (nombre, apellidos, email, password, admin, imagenPerfil) VALUES ('"""+nombre+"""', '"""+apellidos+"""', '"""+email+"""', '"""+password+"""', '"""+admin+"""', '"""+imagenPerfil+"""');"""
 		try:
 			cursor.execute(sql)
 			db.commit()
 		except:
 			db.rollback()
-		db.close()
+		cursor.close()
 		return "Successfully inserted on database."
 	except:
+		cursor.close()
 		return "Unable to insert data on database."
 
 @app.route('/post/incidencias', methods=['POST'])
 def insertar_incidencias():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		descripcion = r.get("descripcion")
@@ -87,21 +116,23 @@ def insertar_incidencias():
 		latitud = r.get("latitud")
 		longitud = r.get("longitud")
 		email = r.get("email")
-		sql = """INSERT INTO Incidencias (descripcion, direccion, imagen, latitud, longitud, email) VALUES ('"""+descripcion+"""', '"""+direccion+"""', '"""+imagen+"""', '"""+latitud+"""', '"""+longitud+"""', '"""+email+"""')"""
+		sql = """INSERT INTO Incidencias (descripcion, direccion, imagen, latitud, longitud, email) VALUES ('"""+descripcion+"""', '"""+direccion+"""', '"""+imagen+"""', '"""+latitud+"""', '"""+longitud+"""', '"""+email+"""');"""
 		try:
 			cursor.execute(sql)
 			db.commit()
 		except:
 			db.rollback()
-		db.close()
+		cursor.close()
 		return "Successfully inserted on database."
 	except:
+		cursor.close()
 		print 'peta'
 		return "Unable to insert data on database."
 
 
 @app.route('/put/usuario', methods=['POST'])
 def actualizar_usuario():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		idi = r.get("id")
@@ -111,15 +142,16 @@ def actualizar_usuario():
 		password = r.get("password")
 		admin = r.get("admin")
 		imagenPerfil = r.get("imagenPerfil")
-		sql = """UPDATE Usuario SET nombre = """+nombre+""", apellidos = """+apellidos+""", email = """+email+""", password = """+password+""", admin = """+admin+""", imagenPerfil = """+imagenPerfil+""" WHERE id ="""+ids
+		sql = """UPDATE Usuario SET nombre = """+nombre+""", apellidos = """+apellidos+""", email = """+email+""", password = """+password+""", admin = """+admin+""", imagenPerfil = """+imagenPerfil+""" WHERE id ="""+ids+""";"""
 		try:
 			cursor.execute(sql)
 			db.commit()
 		except:
 			db.rollback()
-		db.close()
+		cursor.close()
 		return "Successfully inserted on database."
 	except:
+		cursor.close()
 		return "Unable to insert data on database."
 
 
@@ -127,22 +159,24 @@ def actualizar_usuario():
 
 @app.route('/del/incidencias', methods=['POST'])
 def borrar_incidencias():
+	cursor = db.cursor()
 	try:
 		r = request.get_json()
 		idi = r.get("id")
-		sql = """DELETE FROM Incidencias WHERE id="""+idi
+		sql = """DELETE FROM Incidencias WHERE id="""+idi+""";"""
 		try:
 			cursor.execute(sql)
 			db.commit()
 		except:
 			db.rollback()
-		db.close()
+		cursor.close()
 		return "Successfully inserted on database."
 	except:
+		cursor.close()
 		return "Unable to insert data on database."
 
 
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=80)
+	app.run(host='0.0.0.0', debug=True, port=5001, processes=3)
