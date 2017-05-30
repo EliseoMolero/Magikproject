284560b6e1066b57b873a51142d1e1fe4c2c6f93
Ramon Bajona Serna <xuladeja@gmail.com>
2017-05-30 17:19:41 +0200

    Add files via upload

--- Magik_repair/app/src/main/java/com/example/montxu/magik_repair/subirIncidencias.java
+++ Magik_repair/app/src/main/java/com/example/montxu/magik_repair/subirIncidencias.java
@@ -1,10 +1,17 @@
 package com.example.montxu.magik_repair;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -12,6 +19,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,7 +33,10 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+
 import java.io.File;
+import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -50,6 +61,8 @@ public class subirIncidencias extends Fragment {
     private RelativeLayout mRlView;
     private String mPath;
     private EditText mCajaDes;
+    String lat;
+    String lng;
 
     @Nullable
     @Override
@@ -62,7 +75,7 @@ public class subirIncidencias extends Fragment {
         mCajaDes = (EditText) mView.findViewById(R.id.cajaDes);
         Spinner spinner = (Spinner) mView.findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-        R.array.tipos_incidencias, android.R.layout.simple_spinner_item);
+                R.array.tipos_incidencias, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         mOptionButton.setOnClickListener(new View.OnClickListener() {
@@ -76,9 +89,46 @@ public class subirIncidencias extends Fragment {
             @Override
             public void onClick(View v) {
                 //MANDAR TODOO A LA API""!!
-                    String descripcion = String.valueOf(mCajaDes.getText());
-                    HttpPostIncidencias tareaAsync = new HttpPostIncidencias("full",descripcion);
+                String descripcion = String.valueOf(mCajaDes.getText());
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+                }
+                LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    lat = String.valueOf(location.getLatitude());
+                    lng = String.valueOf(location.getLongitude());
+                }
+                // Elemento geocoder en el contexto
+                Geocoder geocoder = new Geocoder(getContext());
+                // Elemento list que contendra la direccion
+                List<Address> direcciones = null;
+
+                // Funcion para obtener coger el nombre desde el geocoder
+                try {
+                    direcciones = geocoder.getFromLocation(location.getLatitude(), location.getLongitude() ,1);
+                } catch (Exception e) {
+                    Log.d("Error", "Error en geocoder:"+e.toString());
+                }
+
+                // Funcion que determina si se obtuvo resultado o no
+                if(direcciones != null && direcciones.size() > 0 ){
+
+                    // Creamos el objeto address
+                    Address direccion = direcciones.get(0);
+
+                    // Creamos el string a partir del elemento direccion
+                    String dic = direccion.getAddressLine(0);
+                    //String direccionText = String.format("%s %s %s",
+                    //        direccion.getMaxAddressLineIndex() > 0 ? direccion.getAddressLine(0) : "",
+                    //        direccion.getLocality(),
+                    //        direccion.getCountryName()
+                    //);
+                    HttpPostIncidencias tareaAsync = new HttpPostIncidencias("full", descripcion,lat, lng, dic);
                     tareaAsync.execute();
+                }
+
 
 
 
@@ -94,9 +144,9 @@ public class subirIncidencias extends Fragment {
         builder.setItems(option, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if(option[which] == "Camara [o]"){
+                if (option[which] == "Camara [o]") {
                     openCamera();
-                }else if (option[which] == "Galeria {||}") {
+                } else if (option[which] == "Galeria {||}") {
                     Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     intent.setType("image/*");
                     startActivityForResult(Intent.createChooser(intent, "Selecciona la foto de la incidencia"), SELECT_PICTURE);
@@ -113,10 +163,10 @@ public class subirIncidencias extends Fragment {
         File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
         boolean isDirectoryCreated = file.exists();
 
-        if(!isDirectoryCreated)
+        if (!isDirectoryCreated)
             isDirectoryCreated = file.mkdirs();
 
-        if(isDirectoryCreated){
+        if (isDirectoryCreated) {
             Long timestamp = System.currentTimeMillis() / 1000;
             String imageName = timestamp.toString() + ".jpg";
 
@@ -141,8 +191,8 @@ public class subirIncidencias extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK){
-            switch (requestCode){
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
                 case PHOTO_CODE:
                     MediaScannerConnection.scanFile(getContext(),
                             new String[]{mPath}, null,
@@ -167,29 +217,39 @@ public class subirIncidencias extends Fragment {
         }
     }
 
-    private class HttpPostIncidencias extends AsyncTask<String, Void, String>{
+
+    public class HttpPostIncidencias extends AsyncTask<String, Void, String> {
 
         String resultado;
         String imagen;
         String descripcion;
-        map_formulario m=new map_formulario();
-        Usuario us=new Usuario();
+        String lat;
+        String lng;
+        String direccion;
+        String estado;
 
-        public HttpPostIncidencias(String imagen, String descripcion) {
+        public HttpPostIncidencias(String imagen, String descripcion, String lat, String lng, String direccion, String estado ) {
             this.imagen = imagen;
             this.descripcion = descripcion;
+            this.lat = lat;
+            this.lng = lng;
+            this.direccion = direccion;
+            this.estado = estado;
         }
 
         @Override
         protected String doInBackground(String... params) {
-            String latitud = String.valueOf(m.getLat());
+
+            String latitud = lng;
             String descripcion =this.descripcion;
-            String direccion="weno";
+            String direccion=this.direccion;
             String imagen=this.imagen;
-            String longitud = String.valueOf(m.getLng());
+            String longitud = lat;
             String email="antonio@mail.com";
+            String estado = "No revisado todavia";
+            System.out.println(direccion);
             System.out.println(latitud + " y " + longitud);
-            operacionesApi.postIncidencia(descripcion, direccion, imagen, latitud, longitud, email);
+            operacionesApi.postIncidencia(descripcion, direccion, imagen, latitud, longitud, email, estado);
 
             return resultado;
         }
