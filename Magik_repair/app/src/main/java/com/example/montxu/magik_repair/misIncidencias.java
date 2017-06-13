1180b5c7932d27b1183d321be36751aacb433846
aguardia13 <er_guardia@hotmail.com>
2017-06-13 11:57:07 +0200

    Update misIncidencias.java

--- Magik_repair/app/src/main/java/com/example/montxu/magik_repair/misIncidencias.java
+++ Magik_repair/app/src/main/java/com/example/montxu/magik_repair/misIncidencias.java
@@ -4,13 +4,19 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+
+import static android.R.*;
 
 
 /**
